f2bcdcecc720f3166a0227c866b01bbd3fda903e
halil_sahin <halil.sahin@solviads.com>
2021-01-22 16:19:25 +0300

    initial commit

---
+++ FileManipulator/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace FileManipulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.btnFileChooser = new System.Windows.Forms.Button();
+            this.btnTransfer = new System.Windows.Forms.Button();
+            this.btnDirChooser = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStartup = new System.Windows.Forms.Button();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(74, 35);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(202, 22);
+            this.txtSource.TabIndex = 0;
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(74, 76);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(202, 22);
+            this.txtDestination.TabIndex = 1;
+            // 
+            // btnFileChooser
+            // 
+            this.btnFileChooser.Location = new System.Drawing.Point(282, 18);
+            this.btnFileChooser.Name = "btnFileChooser";
+            this.btnFileChooser.Size = new System.Drawing.Size(75, 50);
+            this.btnFileChooser.TabIndex = 2;
+            this.btnFileChooser.Text = "Dosya Seç ";
+            this.btnFileChooser.UseVisualStyleBackColor = true;
+            this.btnFileChooser.Click += new System.EventHandler(this.btnFileChooser_Click);
+            // 
+            // btnTransfer
+            // 
+            this.btnTransfer.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnTransfer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTransfer.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTransfer.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnTransfer.Location = new System.Drawing.Point(74, 110);
+            this.btnTransfer.Name = "btnTransfer";
+            this.btnTransfer.Size = new System.Drawing.Size(364, 90);
+            this.btnTransfer.TabIndex = 3;
+            this.btnTransfer.Text = "Transfer Et";
+            this.btnTransfer.UseVisualStyleBackColor = false;
+            this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
+            // 
+            // btnDirChooser
+            // 
+            this.btnDirChooser.Location = new System.Drawing.Point(363, 18);
+            this.btnDirChooser.Name = "btnDirChooser";
+            this.btnDirChooser.Size = new System.Drawing.Size(75, 50);
+            this.btnDirChooser.TabIndex = 4;
+            this.btnDirChooser.Text = "Klasör Seç";
+            this.btnDirChooser.UseVisualStyleBackColor = true;
+            this.btnDirChooser.Click += new System.EventHandler(this.btnDirChooser_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Hedef :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kaynak :";
+            // 
+            // btnStartup
+            // 
+            this.btnStartup.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnStartup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartup.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStartup.Location = new System.Drawing.Point(74, 225);
+            this.btnStartup.Name = "btnStartup";
+            this.btnStartup.Size = new System.Drawing.Size(364, 92);
+            this.btnStartup.TabIndex = 7;
+            this.btnStartup.Text = "Başlangıç İçin Dosya Seç";
+            this.btnStartup.UseVisualStyleBackColor = false;
+            this.btnStartup.Click += new System.EventHandler(this.btnStartup_Click);
+            // 
+            // fileDialog
+            // 
+            this.fileDialog.FileName = "openFileDialog1";
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Location = new System.Drawing.Point(282, 71);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(156, 33);
+            this.btnDestination.TabIndex = 8;
+            this.btnDestination.Text = "Hedef Klasörü Seç";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(463, 388);
+            this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.btnStartup);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDirChooser);
+            this.Controls.Add(this.btnTransfer);
+            this.Controls.Add(this.btnFileChooser);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.txtSource);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Askra";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button btnFileChooser;
+        private System.Windows.Forms.Button btnTransfer;
+        private System.Windows.Forms.Button btnDirChooser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStartup;
+        private System.Windows.Forms.OpenFileDialog fileDialog;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ FileManipulator/Form1.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace FileManipulator
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+            player.URL = "guile.mp3";
+        }
+        OpenFileDialog sourceDialog = new OpenFileDialog();
+        OpenFileDialog exeDialog = new OpenFileDialog();
+        FolderBrowserDialog destinationFolderDialog = new FolderBrowserDialog();
+        FolderBrowserDialog sourceFolderDialog = new FolderBrowserDialog();
+        bool btnFileChooserClickChecker = false;
+
+        private void btnFileChooser_Click(object sender, EventArgs e)
+        {
+            btnFileChooserClickChecker = true;
+            if (sourceDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSource.Text = sourceDialog.FileName;
+            }
+        }
+
+        private void btnTransfer_Click(object sender, EventArgs e)
+        {
+            if(btnFileChooserClickChecker)
+            {
+                File.Copy(txtSource.Text, txtDestination.Text + "\\" + sourceDialog.SafeFileName, true);
+                MessageBox.Show("AKTARIM BAŞARIYLA TAMAMLANDI");
+            }
+            else
+            {
+                String[] fileList = Directory.GetFiles(sourceFolderDialog.SelectedPath);
+
+                foreach (String filePath in fileList)
+                {
+                    File.Copy(filePath, txtDestination.Text + "\\" + Path.GetFileName(filePath), true);
+                }
+                    MessageBox.Show("AKTARIM BAŞARILIYLA TAMAMLANDI");
+            }
+        }
+
+        private void btnDestination_Click(object sender, EventArgs e)
+        {
+            if (destinationFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtDestination.Text = destinationFolderDialog.SelectedPath;
+            }
+        }
+
+        private void btnDirChooser_Click(object sender, EventArgs e)
+        {
+            if(sourceFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSource.Text = sourceFolderDialog.SelectedPath;
+            }
+        }
+
+        private void btnStartup_Click(object sender, EventArgs e)
+        {
+            exeDialog.Filter = "Başlangıç Dosyaları (.exe;.bat;.vbs)|*.exe;*.bat;*.vbs|All files|*.*";
+            if(exeDialog.ShowDialog() == DialogResult.OK)
+            {
+                RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                reg.SetValue(exeDialog.SafeFileName, exeDialog.FileName);
+                MessageBox.Show("DOSYA BAŞLANGIÇTA ÇALIŞMAK İÇİN KAYDEDİLDİ");
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            bool mousePointerNotOnTaskBar = Screen.GetWorkingArea(this).Contains(Cursor.Position);
+            if(this.WindowState == FormWindowState.Minimized && mousePointerNotOnTaskBar)
+            {
+                notifyIcon1.ShowBalloonTip(1000, "Askra Bildirimi", "Uygulama Sistem Tepsisine Küçültüldü", ToolTipIcon.Info);
+                this.ShowInTaskbar = false;
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            if(this.WindowState == FormWindowState.Normal)
+            {
+                this.ShowInTaskbar = true;
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player.controls.play();
+        }
+    }
+}
